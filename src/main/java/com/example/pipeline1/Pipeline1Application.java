e2b53294915c0e08491643d4f9585a5da5c2620c
gottbrecht <simo79j5@stud.kea.dk>
2023-10-19 21:58:19 +0200

    pipeline setup

---
+++ src/main/java/com/example/pipeline1/Pipeline1Application.java
@@ -0,0 +1,13 @@
+package com.example.pipeline1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pipeline1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pipeline1Application.class, args);
+    }
+
+}
